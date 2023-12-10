f6b3df597a75a0a70ef404f29e5ca43258b9449f
spacecreator8 <spcc888@ya.ru>
2023-12-10 07:06:35 +0700

    Регистрация и авторизация(часть)

--- main/social_network/views.py
+++ main/social_network/views.py
@@ -26,3 +26,4 @@ class LoginViewMy(LoginView):
     form_class = AuthenticationForm
     template_name = 'social_network/login.html'
     extra_context = {'title':'Авторизация'}
+    success_url = reverse_lazy('main:index')
